dfb27bbaa4d06f0e3afd3e7722dbf9b297af8842
cowankc <cowankc92@live.com>
2019-11-02 23:43:22 -0500

    ran npm install again

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,7 @@ let mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createPool({
-    connectionLimit: 15,
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -12,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
 });
 }
-connection.getConnection(function(err, connection) {
+connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
     return;
